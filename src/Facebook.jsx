8743776d71ac7fb3d27d754239393c255c605339
AsadullahAbbasi <asadullahabbasi225@gmail.com>
2024-04-24 00:56:45 +0500

    login with fb

--- src/Facebook.jsx
+++ src/Facebook.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
-
-
+import { getAuth, signInWithPopup, FacebookAuthProvider ,onAuthStateChanged } from "firebase/auth";
+import app from './firebase';
+import { useEffect } from 'react';
 const Facebook = () => {
-const handleFb = ()=>{
-   
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { displayName, email, emailVerified, uid } = user;
+        console.log(
+          "User is signed in:",
+          displayName,
+          email,
+          emailVerified,
+          uid,
+          user
+        );
+
+        if (emailVerified) {
+          console.log("Email is verified!");
+         
+        } else {
+          console.log("Email is not verified.");
+        }
+      } 
+    });
+
+    return () => unsubscribe();
+  });
+const handleFb = (e)=>{
+  e.preventDefault()
+  const auth = getAuth(app);
+  const provider = new FacebookAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+  
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+   console.log(user);
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+  
+      // ...
+    });
 }
 
   return (
